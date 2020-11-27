0491e53e410e31f2d7f29c2811e39b86511a2e26
vkssingh892354@gmail.com <65289999+vkssingh8923@users.noreply.github.com>
2020-11-27 07:26:58 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import uuid from "uuid";
+import './App.scss'
+import "bootstrap/dist/css/bootstrap.min.css";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+// showing vs-code github setup
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="r1ow">
+            <TodoInput />
+            <TodoList />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
